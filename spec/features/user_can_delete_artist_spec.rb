9bd1eb8a0a699ca3154441e5245c85e65a2e7678
marinacor1 <marinaspam1234@gmail.com>
2016-04-12 21:26:33 -0600

    delete test written

---
+++ spec/features/user_can_delete_artist_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "user can delete artist" do
+  scenario "user sees index page of artist" do
+    artist_1 = "Bob Marley"
+    url = "https://s-media-cache-ak0.pinimg.com/736x/b0/d7/a3/b0d7a3554f5dc903e45b6bedbb0a4010.jpg"
+
+    artist = Arist.create(name: artist_1, image_path: url)
+
+    visit 'artist_path(artist)'
+
+    click_on "Delete"
+
+    expect(page).to not_have_content artist_1
+    expect(page).to not_have_css("img[src=\"#{url}\"]")
+  end
+end
