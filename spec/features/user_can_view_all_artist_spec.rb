9af3396b0939d31c7ef4ca7740d5e5f1bde414fe
marinacor1 <marinaspam1234@gmail.com>
2016-04-12 20:43:39 -0600

    test for index passing

---
+++ spec/features/user_can_view_all_artist_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "user can view all artists on one page" do
+  scenario "list of all artists shows" do
+    artist_1 = "Bob Marley"
+    url = "https://s-media-cache-ak0.pinimg.com/736x/b0/d7/a3/b0d7a3554f5dc903e45b6bedbb0a4010.jpg"
+    artist_2 = "Selena"
+    url_2 = "http://media2.fdncms.com/sacurrent/imager/adoration-for-tejano-music-superstar-selen/u/original/2413220/selena-quintanilla-1.jpg"
+
+    Artist.create(name: artist_1, image_path: url)
+    Artist.create(name: artist_2, image_path: url_2)
+    visit artists_path
+    expect(page).to have_content artist_1
+    expect(page).to have_css("img[src=\"#{url}\"]")
+    expect(page).to have_content artist_2
+    expect(page).to have_css("img[src=\"#{url_2}\"]")
+
+
+  end
+end
