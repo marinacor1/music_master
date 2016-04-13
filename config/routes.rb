2556846ef0f75d88883711eadc5ea6b1f13abb86
marinacor1 <marinaspam1234@gmail.com>
2016-04-12 21:54:20 -0600

    table for songs created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do
+    resources :songs 
+    end
 end
---
+++ spec/features/user_can_create_song_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "user can create a song" do
+  scenario "user can see new song in individual page" do
+    artist_1 = "Bob Marley"
+    url = "https://s-media-cache-ak0.pinimg.com/736x/b0/d7/a3/b0d7a3554f5dc903e45b6bedbb0a4010.jpg"
+
+    artist_bob = Artist.create(name: artist_1, image_path: url)
+    song_title = "One Love"
+
+    visit artist_path(artist_bob)
+    click_on "Create Song"
+
+    expect(page).to have_content song_title
+    expect(page).to have_link artist_bob.name, href: artist_path(artist_bob)
+  end
+end
