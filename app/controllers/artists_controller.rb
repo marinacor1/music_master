45193cc5ce327cefb18deae7d0de6543e3cf3783
marinacor1 <marinaspam1234@gmail.com>
2016-04-12 21:23:10 -0600

    edit tests pass

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -20,6 +20,19 @@ class ArtistsController < ApplicationController
       end
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(param_check)
+      redirect_to artist_path(@artist)
+    else
+      render :edit
+    end
+  end
+
   private
   def param_check
     params.require(:artist).permit(:name, :image_path)
